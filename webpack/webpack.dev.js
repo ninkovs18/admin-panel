81695f115649a33c98d2b86ae66eefb592497c01
Strahinja1520 <strahinja1520@its.edu.rs>
2024-06-08 21:58:48 +0200

    set react-refresh-plugin

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,4 +1,11 @@
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+
 module.exports = {
   mode: "development",
+  devServer: {
+    hot: true,
+    open: true,
+  },
   devtool: "cheap-module-source-map",
+  plugins: [new ReactRefreshPlugin()],
 };
