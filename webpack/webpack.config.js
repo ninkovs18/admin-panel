c125b7a17365ef66c742f9320bff1aaf5855bd58
Strahinja1520 <strahinja1520@its.edu.rs>
2024-06-08 21:41:39 +0200

    allow css files & images

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -14,6 +14,18 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.css?$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
     ],
   },
   output: {
