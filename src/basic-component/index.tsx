30ee2d13be891d6e26a1905847aded9fcf291381
Strahinja1520 <strahinja1520@its.edu.rs>
2024-06-09 18:54:44 +0200

    install Fluent UI 9

--- src/basic-component/index.tsx
+++ src/basic-component/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 
+import { Button } from '@fluentui/react-components'
+
 const BasicComponent: React.FC = () => {
   const [count, setCount] = useState(0)
   return (
     <>
       <h1>REACT BASE !!!</h1>
-      <button onClick={() => setCount(c => c + 1)}>Count: {count}</button>
+      <Button onClick={() => setCount(c => c + 1)}>Count {count}</Button>
     </>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { FluentProvider, webLightTheme } from '@fluentui/react-components'
 import App from './router/'
 
 const container = document.getElementById('root')
@@ -14,7 +15,7 @@ const router = createBrowserRouter([
 ])
 
 root.render(
-  <React.StrictMode>
+  <FluentProvider theme={webLightTheme}>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </FluentProvider>,
 )
